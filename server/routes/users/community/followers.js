31b21ee099b93083aa4ffb109bb8b24f8b7a45ae
David Shau <davidshau22@berkeley.edu>
2020-07-08 21:29:11 -0700

    removing follower bug fixed

--- server/routes/users/community/followers.js
+++ server/routes/users/community/followers.js
@@ -27,7 +27,8 @@ const acceptFollowerRequest = async (req, res) => {
   // 1.
   console.log(userToken, SECRET)
   try {
-    var receiverID = await jwt.verify(userToken, SECRET)
+    const receiver = await jwt.verify(userToken, SECRET)
+    var receiverID = receiver._id
   } catch(e) {
     return sendError(res, e)
   }
@@ -93,10 +94,63 @@ const rejectFollowerRequest = (req, res) => {
 }
 
 // removes a follower from followers list
-const removeFollower = (req, res) => {
-  res.send({
-    success: true
-  })
+const removeFollower = async (req, res) => {
+  console.log('removing follower')
+  // 1. verify user token
+  // 2. add to receiver's followers, remove from receiver's follwerRequests
+  // 3. add to requester's following, remove from requester's followingPending
+  // 4. send success response
+
+  // cuz i was lazy and wanted to use the same request object, the receiver here is
+  // the person who did the removing. The requester is the person who was removed
+  var {
+    userToken,
+    requesterID,
+    requesterFirstName,
+    requesterLastName,
+  } = req.body
+
+  // 1.
+  try {
+    const remover = await jwt.verify(userToken, SECRET)
+    var removerID = remover._id
+  } catch(e) {
+    return sendError(res, e)
+  }
+
+  // start session for transaction
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    const removedUser = await User.findOne({ _id: requesterID }).session(session);
+    // remove from the removed user's following list
+    removedUser.following = removedUser.following.filter(user => {
+      return user._id !== removerID
+    })
+    await removedUser.save();
+
+    const remover = await User.findOne({ _id: removerID }).session(session);
+    // remove from the remover's followers list
+    remover.followers = remover.followers.filter(user => {
+      return user._id !== requesterID
+    })
+    await remover.save();
+
+    // commit the changes if everything was successful
+    await session.commitTransaction();
+    // send success response back to client
+    res.send({
+      success: true,
+      message: `${requesterFirstName} ${requesterLastName} no longer follows you`
+    })
+  } catch(e) {
+    console.error(e);
+    // this will rollback any changes made in the database
+    await session.abortTransaction();
+    return sendError(res, e)
+  } finally {
+    session.endSession();
+  }
 }
 
 const followers = {
